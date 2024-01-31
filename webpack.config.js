0aee68a37e94e53c13c759d0326a9cf7b4255770
HannaTsilikina <https://github.com/HannaTsilikina/Practicum17.git>
2024-01-31 21:30:51 +0300

    update

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,21 @@
+import webpack from "webpack";
+
+// Try the environment variable, otherwise use root
+const ASSET_PATH = process.env.ASSET_PATH || "./";
+
+export default {
+  output: {
+    publicPath: "./",
+  },
+
+  plugins: [
+    // This makes it possible for us to safely use env vars on our code
+    new webpack.DefinePlugin({
+      "process.env.ASSET_PATH": JSON.stringify(ASSET_PATH),
+    }),
+  ],
+};
+
 module.exports = {
   module: {
     rules: [
