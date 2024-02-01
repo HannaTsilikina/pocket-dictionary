6932f0e00aebcbc1c786b0ea50045d480839850a
HannaTsilikina <https://github.com/HannaTsilikina/Practicum17.git>
2024-02-02 00:27:56 +0300

    training was added

--- src/components/TrainMode/TrainMode.jsx
+++ src/components/TrainMode/TrainMode.jsx
@@ -1,8 +1,7 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import ReactCardFlip from "react-card-flip";
 import "./TrainMode.scss";
 import LearnCard from "../LearnCard/LearnCard";
-
 import "../../assets/images/right-arrow.png";
 import listOfWords from "../../store/store";
 import arrowRight from "../../assets/images/right-arrow.png";
@@ -11,10 +10,25 @@ import arrowLeft from "../../assets/images/left-arrow.png";
 export default function TrainMode(props) {
   const [isFlipped, setFlipped] = useState(false);
   const [number, setNumber] = useState(props.number);
+  const [learned, setLearned] = useState(props.learned);
+  const [CountOfLearnedWords, setCountOfLearnedWords] = useState(0);
+  const [training, setTraining] = useState(false);
+
+  // const ref = useRef(null);
+
+  // useEffect(() => {
+  //   if (ref.current) {
+  //     console.log(ref.current);
+  //     ref.current.focus();
+  //   }
+  // }, [number]);
+
   const handleChangeNumber = () => {
     setFlipped(false);
-    if (number >= props.array.length - 1) setNumber(props.number);
-    else setNumber(number + 1);
+    if (number >= props.array.length - 1) {
+      setNumber(props.number);
+      setTraining(true);
+    } else setNumber(number + 1);
   };
   const handleChangeNumberPrev = () => {
     setFlipped(false);
@@ -25,33 +39,57 @@ export default function TrainMode(props) {
 
   const handleChangeFlipped = () => {
     setFlipped(!isFlipped);
+
+    setLearned(true);
+    if (CountOfLearnedWords < 12) {
+      setCountOfLearnedWords(CountOfLearnedWords + 1);
+    }
   };
-  return (
+  return training ? (
     <main className="trainmode-main">
+      <h2 className="text-finish">
+        The training is over. <br /> Your result : {CountOfLearnedWords} learned
+        words
+      </h2>
+      <a href="/">
+        <button className="button-save">{"Back to dictionary"}</button>
+      </a>
+    </main>
+  ) : (
+    <main className="trainmode-main">
+      <h1>Training</h1>
       <div className="trainmode-container">
-        <button className="button-secondary" onClick={handleChangeNumberPrev}>
-          <img className="train-arr" src={arrowLeft} alt="arrow" />
-        </button>
-        <div className="react-card-flip">
-          <ReactCardFlip
-            isFlipped={isFlipped}
-            flipSpeedFrontToBack={0.4}
-            flipDirection="horizontal"
-          >
-            <LearnCard
-              card={listOfWords[number]}
-              onClick={handleChangeFlipped}
-            />
-            <LearnCard card={listOfWords[number]} checked={true} />
-          </ReactCardFlip>
-        </div>
-        <button className="button-secondary" onClick={handleChangeNumber}>
-          <img className="train-arr" src={arrowRight} alt="arrow" />
-        </button>
+        <>
+          <button className="button-secondary" onClick={handleChangeNumberPrev}>
+            <img className="train-arr" src={arrowLeft} alt="arrow" />
+          </button>
+
+          <div className="react-card-flip">
+            <ReactCardFlip
+              isFlipped={isFlipped}
+              flipSpeedFrontToBack={0.4}
+              flipDirection="horizontal"
+            >
+              <LearnCard
+                card={listOfWords[number]}
+                onClick={handleChangeFlipped}
+                learned={learned}
+                // ref={ref}
+              />
+              <LearnCard card={listOfWords[number]} checked={true} />
+            </ReactCardFlip>
+          </div>
+          <button className="button-secondary" onClick={handleChangeNumber}>
+            <img className="train-arr" src={arrowRight} alt="arrow" />
+          </button>
+        </>
       </div>
 
-      <span className="TrainMode-counter">
+      <span className="trainMode-counter">
         {number + 1}/{props.array.length}
+      </span>
+      <span className="learned-words-count">
+        Learned {CountOfLearnedWords} words
       </span>
     </main>
   );
