3e6633b26b3a970b0c2963f136c481345233ccc1
HannaTsilikina <https://github.com/HannaTsilikina/Practicum17.git>
2024-02-24 18:41:58 +0300

    inputs errors were changed

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,12 +1,14 @@
 import { useEffect, useState } from "react";
 import Button from "../AppUI/Buttons/Button";
 
+// проверка на транскрипцию, кириллицу и пароль!
+
 function Card(props) {
   const [errorValues, setErrorValue] = useState({
-    name: false,
-    translation: false,
-    transcription: false,
-    topic: false,
+    inputName: false,
+    inputTranscription: false,
+    inputTranslation: false,
+    inputTopic: false,
   });
   const [edit, setEdit] = useState(false);
   const handleEdit = () => {
@@ -19,36 +21,56 @@ function Card(props) {
   const handleSave = () => {
     setEdit(!edit);
     console.log(
-      `name: ${name}, transcription: ${transcription}, translation: ${translation}, topic: ${topic}`
+      `name: ${inputs.inputName}, transcription: ${inputs.inputTranscription}, translation: ${inputs.inputTranslation}, topic: ${inputs.inputTopic}`
     );
   };
-  const [transcription, setTranscription] = useState(props.transcription);
-  const [translation, setTranslation] = useState(props.translation);
-  const [topic, setTopic] = useState(props.topic);
-  const [name, setName] = useState(props.name);
+  const [inputs, setInputState] = useState({
+    inputName: props.name,
+    inputTranscription: props.transcription,
+    inputTranslation: props.translation,
+    inputTopic: props.topic,
+  });
   const [errorMessage, setErrorMessage] = useState("");
 
   useEffect(() => {
     if (Object.values(errorValues).includes(true)) {
-      setErrorMessage(`Please enter the correct`);
+      let wrongInputs = [];
+      for (let i = 0; i < Object.values(errorValues).length; i++) {
+        if (Object.values(errorValues)[i] === true) {
+          if (Object.keys(errorValues)[i] === "inputName") {
+            wrongInputs.push("english word");
+          }
+          if (Object.keys(errorValues)[i] === "inputTranscription") {
+            wrongInputs.push("transcription");
+          }
+          if (Object.keys(errorValues)[i] === "inputTranslation") {
+            wrongInputs.push("translation");
+          }
+          if (Object.keys(errorValues)[i] === "inputTopic") {
+            wrongInputs.push("topic");
+          }
+        }
+      }
+
+      setErrorMessage(`Please enter the correct ${wrongInputs.join(",")} `);
     } else {
       setErrorMessage("");
     }
   }, [errorValues]);
 
-  const handleInputChange = (e, inputName, setInputState) => {
-    if (e.target.value.trim() === "" && errorValues[inputName] === false) {
+  const handleInputChange = (e, stringOfKey, setInputState) => {
+    if (e.target.value.trim() === "" && errorValues[stringOfKey] === false) {
       setErrorValue({
         ...errorValues,
-        [inputName]: true,
+        [stringOfKey]: true,
       });
     } else {
       setErrorValue({
         ...errorValues,
-        [inputName]: false,
+        [stringOfKey]: false,
       });
     }
-    setInputState(e.target.value);
+    setInputState({ ...inputs, [stringOfKey]: e.target.value });
   };
 
   if (!props.new && props.main)
@@ -67,46 +89,48 @@ function Card(props) {
         <div className="card-container" key={props.id}>
           <input
             className={
-              errorValues.name === true ? "added-word-error" : "added-word"
+              errorValues.inputName === true ? "added-word-error" : "added-word"
             }
-            value={name}
-            onChange={(e) => handleInputChange(e, "name", setName)}
+            value={inputs.inputName}
+            onChange={(e) => handleInputChange(e, "inputName", setInputState)}
           ></input>
           <input
             className={
-              errorValues.transcription === true
+              errorValues.inputTranscription === true
                 ? "added-word-error"
                 : "added-word"
             }
-            value={transcription}
+            value={inputs.inputTranscription}
             onChange={(e) =>
-              handleInputChange(e, "transcription", setTranscription)
+              handleInputChange(e, "inputTranscription", setInputState)
             }
           ></input>
           <input
             className={
-              errorValues.translation === true
+              errorValues.inputTranslation === true
                 ? "added-word-error"
                 : "added-word"
             }
-            value={translation}
+            value={inputs.inputTranslation}
             onChange={(e) =>
-              handleInputChange(e, "translation", setTranslation)
+              handleInputChange(e, "inputTranslation", setInputState)
             }
           ></input>
           <input
             className={
-              errorValues.topic === true ? "added-word-error" : "added-word"
+              errorValues.inputTopic === true
+                ? "added-word-error"
+                : "added-word"
             }
-            value={topic}
-            onChange={(e) => handleInputChange(e, "topic", setTopic)}
+            value={inputs.inputTopic}
+            onChange={(e) => handleInputChange(e, "inputTopic", setInputState)}
           ></input>
           <div className="buttons-container">
             <Button
               class="button-save"
               text="Save"
               onclick={handleSave}
-              disabledButton={errorValues}
+              disabledButton={Object.values(errorValues).includes(true)}
             />
             <Button
               class="button-delete"
@@ -122,10 +146,10 @@ function Card(props) {
     return (
       <div className="card-main">
         <div className="card-container" key={props.id}>
-          <h3>{name}</h3>
-          <h3>{transcription}</h3>
-          <h3> {translation}</h3>
-          <h4> {topic}</h4>
+          <h3>{inputs.inputName}</h3>
+          <h3>{inputs.inputTranscription}</h3>
+          <h3> {inputs.inputTranslation}</h3>
+          <h4> {inputs.inputTopic}</h4>
           <div className="buttons-container">
             <Button class="button-change" text="Change" onclick={handleEdit} />
             <Button
